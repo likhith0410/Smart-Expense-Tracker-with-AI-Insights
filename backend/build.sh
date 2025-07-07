b604adc0903d13c918ec57e354a335ddf5a55d98
Likhith Gowda T R <155158261+likhith0410@users.noreply.github.com>
2025-07-07 12:38:26 +0530

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 apt-get update
-apt-get install -y tesseract-ocr tesseract-ocr-eng
+apt-get install -y tesseract-ocr tesseract-ocr-eng libtesseract-dev
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
